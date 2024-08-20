f2f4053c69e443e0012860818c29bf3d5f959074
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-20 09:57:49 +0800

    feat: add UI to correct classify result

--- server.ts
+++ server.ts
@@ -4,12 +4,13 @@ import { main as train } from './train'
 import { main as retrain } from './retrain'
 import { main as classify } from './classify'
 import { main as unclassify } from './unclassify'
-import { readdirSync } from 'fs'
 import { join } from 'path'
+import { readdir, rename } from 'fs/promises'
 
 let app = express()
 
 app.use('/data-template', express.static('node_modules/data-template'))
+app.use('/classified', express.static('classified'))
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -39,27 +40,62 @@ for (let [name, fn] of Object.entries(actions)) {
 /* stats */
 /*********/
 
-function countDirFiles(dir: string) {
-  return readdirSync(dir).length
+async function countDirFiles(dir: string) {
+  let filenames = await readdir(dir)
+  return filenames.length
 }
 
-function countDir2Files(dir: string) {
-  let filenames = readdirSync(dir)
+async function countDir2Files(dir: string) {
+  let filenames = await readdir(dir)
   let total = 0
   for (let filename of filenames) {
-    total += countDirFiles(join(dir, filename))
+    total += await countDirFiles(join(dir, filename))
   }
   return total
 }
 
-app.get('/stats', (req, res) => {
+app.get('/stats', async (req, res) => {
   res.json({
     stats: {
-      dataset: countDir2Files('dataset'),
-      classified: countDir2Files('classified'),
-      unclassified: countDirFiles('unclassified'),
+      dataset: await countDir2Files('dataset'),
+      classified: await countDir2Files('classified'),
+      unclassified: await countDirFiles('unclassified'),
     },
   })
+})
+
+/************/
+/* classify */
+/************/
+
+async function scanDir2Files(dir: string) {
+  let classNames = await readdir(dir)
+  return Promise.all(
+    classNames.map(async className => {
+      return {
+        className,
+        filenames: await readdir(join(dir, className)),
+      }
+    }),
+  )
+}
+
+app.get('/classified', async (req, res) => {
+  res.json({ classes: await scanDir2Files('classified') })
+})
+
+app.post('/classified/correct', async (req, res) => {
+  try {
+    let { className, images } = req.body
+    for (let image of images) {
+      let srcFile = join('classified', image.className, image.filename)
+      let destFile = join('dataset', className, image.filename)
+      await rename(srcFile, destFile)
+    }
+    res.json({})
+  } catch (error) {
+    res.json({ error: String(error) })
+  }
 })
 
 let port = 8100

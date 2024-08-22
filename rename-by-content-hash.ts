520a9748399f6797c80f15d895a6c1875fe3d2a8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-22 11:47:17 +0800

    chore: show progress when rename by content hash

--- rename-by-content-hash.ts
+++ rename-by-content-hash.ts
@@ -1,12 +1,18 @@
 import { readdir } from 'fs/promises'
 import { basename, join } from 'path'
-import { isContentHash, renameFileByContentHash } from 'tensorflow-helpers'
+import {
+  isContentHash,
+  renameFileByContentHash,
+} from 'tensorflow-helpers/dist/file'
 import { db } from './db'
+import { startTimer } from '@beenotung/tslib/timer'
 
 export async function main() {
+  console.log('rename by content hash: scanning directories...')
   await scanDir2('dataset')
   await scanDir2('classified')
   await scanDir('unclassified')
+  console.log('rename by content hash: done.')
 }
 
 async function scanDir2(dir: string) {
@@ -27,10 +33,13 @@ where filename = :old_filename
 
 async function scanDir(dir: string) {
   let filenames = await readdir(dir)
+  filenames = filenames.filter(filename => !isContentHash(filename))
+  if (filenames.length == 0) {
+    return
+  }
+  let timer = startTimer('scan dir ' + JSON.stringify(dir))
+  timer.setEstimateProgress(filenames.length)
   for (let filename of filenames) {
-    if (isContentHash(filename)) {
-      continue
-    }
     let file = join(dir, filename)
     let newFile = await renameFileByContentHash(file)
     let newFilename = basename(newFile)
@@ -38,7 +47,9 @@ async function scanDir(dir: string) {
       new_filename: newFilename,
       old_filename: filename,
     })
+    timer.tick()
   }
+  timer.end()
 }
 
 if (process.argv[1] == __filename) {

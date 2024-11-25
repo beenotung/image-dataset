afda79af08101ab27af99852c00202d239e74594
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-11-25 16:16:11 +0800

    feat: add script to archive image dataset and model

---
+++ scripts/archive.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+find -empty -delete
+
+while true; do
+  sqlite3 db.sqlite3 ".schema" > /dev/null
+   if [ ! -f db.sqlite3-wal ]; then
+    break
+   fi
+  echo "waiting for db.sqlite3-wal to be deleted"
+  sleep 1
+done
+
+read -p "name: " name
+
+mkdir -p models
+
+zip -r "models/$name.zip" \
+  downloaded \
+  dataset \
+  classified \
+  unclassified \
+  db.sqlite3 \
+  list.txt \
+
+echo "archived to models/$name.zip"
+
+rm -rf \
+  downloaded \
+  dataset \
+  classified \
+  unclassified \
+  db.sqlite3 \
+  list.txt \
